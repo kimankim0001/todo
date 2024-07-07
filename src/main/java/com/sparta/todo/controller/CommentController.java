a503756027a873845188005671ea2515d7c70a35
kinam <maltize9292@gmail.com>
2024-07-08 03:53:31 +0900

    feat/선택댓글수정 기능구현

--- src/main/java/com/sparta/todo/controller/CommentController.java
+++ src/main/java/com/sparta/todo/controller/CommentController.java
@@ -22,16 +22,24 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/todos/{todoId}/comments")
-    public ResponseEntity<CommonResDto<CommentResDto>> createComment (
+    public ResponseEntity<CommonResDto<CommentResDto>> createComment(
             @PathVariable(name = "todoId") long todoId, @Valid @RequestBody CommentReqDto commentReqDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         CommentResDto resDto = commentService.createComment(todoId, userDetails.getUser(), commentReqDto);
         return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK.value(), "댓글 작성에 성공하였습니다!", resDto));
     }
 
     @GetMapping("/todos/{todoId}/comments")
-    public ResponseEntity<CommonResDto<List<CommentResDto>>> findAllByTodoIdWithPageAndSortDesc (
+    public ResponseEntity<CommonResDto<List<CommentResDto>>> findAllByTodoIdWithPageAndSortDesc(
             @PathVariable(name = "todoId") long todoId, @RequestParam("page") int page) {
         List<CommentResDto> commentResDtos = commentService.findAllByTodoIdWithPageAndSortDesc(todoId, page, 5);
         return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK.value(), "전체 댓글 조회에 성공하였습니다!", commentResDtos));
     }
+
+    @PatchMapping("/todos/{todoId}/comments/{commentId}")
+    public ResponseEntity<CommonResDto<CommentResDto>> updateComment(
+            @PathVariable(name = "todoId") long todoId, @PathVariable(name = "commentId") long commentId, @Valid @RequestBody CommentReqDto commentReqDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CommentResDto resDto = commentService.updateComment(todoId, commentId, commentReqDto, userDetails.getUser());
+        return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK.value(), "댓글 수정에 성공하였습니다!", resDto));
+    }
+
 }
--- src/main/java/com/sparta/todo/service/CommentService.java
+++ src/main/java/com/sparta/todo/service/CommentService.java
@@ -7,11 +7,13 @@ import com.sparta.todo.entity.Todo;
 import com.sparta.todo.entity.User;
 import com.sparta.todo.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +22,7 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final TodoService todoService;
+    private final MessageSource messageSource;
 
     @Transactional
     public CommentResDto createComment(long todoId, User user, CommentReqDto commentReqDto) {
@@ -29,7 +32,7 @@ public class CommentService {
     }
 
     @Transactional(readOnly = true)
-    public List<CommentResDto> findAllByTodoIdWithPageAndSortDesc (long todoId, int page, int pageSize) {
+    public List<CommentResDto> findAllByTodoIdWithPageAndSortDesc(long todoId, int page, int pageSize) {
         PageRequest pageRequest = PageRequest.of(page - 1, pageSize);
         return commentRepository.findAllByTodoIdWithPageAndSortDesc(todoId, pageRequest.getOffset(), pageRequest.getPageSize())
                 .stream()
@@ -42,5 +45,36 @@ public class CommentService {
                                 .createdAt(comment.getCreatedAt())
                                 .build())
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentResDto updateComment(long todoId, long commentId, CommentReqDto commentReqDto, User user) {
+        todoService.findTodo(todoId);
+        Comment comment = findComment(commentId);
+        String loginUsername = user.getUsername();
+        String commentUsername = comment.getUser().getUsername();
+
+        if (!loginUsername.equals(commentUsername)) {
+            throw new IllegalArgumentException(messageSource.getMessage(
+                    "author.and.user.mismatch",
+                    null,
+                    "User Mismatch",
+                    Locale.getDefault()
+            ));
+        }
+
+        comment.updateComment(commentReqDto.getContent());
+
+        return new CommentResDto(comment.getId(), comment.getContent(), comment.getUser().getUsername(), comment.getTodo().getId(), comment.getCreatedAt());
+    }
+
+    public Comment findComment(long commentId) {
+        return commentRepository.findById(commentId).orElseThrow(
+                () -> new IllegalArgumentException(messageSource.getMessage(
+                        "not.found.comment",
+                        null,
+                        "Not Found Comment",
+                        Locale.getDefault()
+                )));
     }
 }
